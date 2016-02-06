2005c0a6ca43ad52b70ab25577ed559149cede5b
Robert Stevens <robert@stevens.tk>
2016-02-06 03:16:03 +0100

    added lombok

--- src/dataContainers/Charger.java
+++ src/dataContainers/Charger.java
@@ -1,19 +1,30 @@
 /**
- * 
+ * https://projectlombok.org/features/GetterSetter.html
  */
 package dataContainers;
 import dataTypes.*;
 
+import lombok.Getter;
+
 /**
  * @author ing. R.J.H.M. Stevens
+ * @date 6-feb-2016
  *
- */
+ */ 
 public class Charger {
-	private int id;
-	private float longitude;
-	private float latitude;
-	private Connectors connector = Connectors.unknown;
-	private Status status = Status.unknown; 
-	private String notes;
-	private int companyId;
+	/**
+	 * The id of the charger
+	 * @param returns the internal charger id
+	 */
+	@Getter private int id;
+	
+	/**
+	 * 
+	 */
+	@Getter private float longitude;
+	@Getter private float latitude;
+	@Getter private Connectors connector = Connectors.unknown;
+	@Getter private Status status = Status.unknown; 
+	@Getter private String notes;
+	@Getter private int companyId;
 }
